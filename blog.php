44ad6841616220682aeb7429fbe970b7d69566c7
MaxNoka <maxnoka@me.com>
2020-08-16 17:22:02 +0100

    support for searching blog posts by tag

--- blog.php
+++ blog.php
@@ -3,11 +3,13 @@ require('config.php');
 
 $show_all_posts = isset($_GET['show_all_posts']) ? True : False;
 
+$search_tag = (isset($_GET['search_tag'])) ? htmlspecialchars( $_GET['search_tag'] ) : NULL;
+
 $post_no = (isset($_GET['post_no']) && ctype_digit(strval($_GET['post_no']))) ? intval( $_GET['post_no'] ) : NULL;
 
-$blog = new Blog($post_no);
+$blog = new Blog($post_no, $search_tag);
 
-if ($show_all_posts == True) { 
+if ($show_all_posts == True || $search_tag !== NULL) {
 	$view = new MyView($content_file = NULL, $sub_template = 'blog_post_list.html');
 }
 else { // i.e. show and individual post
--- classes/BlogClass.php
+++ classes/BlogClass.php
@@ -10,12 +10,13 @@ require_once(CLASS_PATH . '/' . 'BlogPost.php');
 class Blog {
   public $all_blog_posts_names = NULL;
   public $current_blog_post_index = NULL;
+  public $search_tag = NULL;
 
   public $all_blog_posts = array();
 
   private $blog_post_list_file= CONTENT_PATH . '/blog_posts/_active_blog_posts.txt';
 
-  public function __construct($post_no) {
+  public function __construct($post_no, $search_tag) {
     $txt_file = file_get_contents($this->blog_post_list_file);
 
     $this->all_blog_posts_names = explode("\n", $txt_file);
@@ -31,6 +32,8 @@ class Blog {
     if ( $post_no !==  NULL) {
       $this->change_current_blog_post($post_no);
     }
+
+    $this->search_tag = $search_tag;
   }
 
   public function change_current_blog_post($target_blog_post_index) {
@@ -65,5 +68,15 @@ class Blog {
 
   public function get_current_blog_post() {
     return $this->all_blog_posts[$this->current_blog_post_index];
+  }
+
+  public function get_posts_with_tag($search_tag) {
+    $filtered_posts = array();
+    foreach ($this->all_blog_posts as $blog_post) {
+      if (in_array($search_tag, $blog_post->tags)) {
+        $filtered_posts[] = $blog_post;
+      }
+    }
+    return $filtered_posts;
   }
 }
