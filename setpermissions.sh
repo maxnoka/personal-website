6007cf6a088c054131bb483515fc2458ce6c4f8e
MaxNoka <maxnoka@me.com>
2021-01-10 16:44:39 +0000

    remove futoshiki path

--- setpermissions.sh
+++ setpermissions.sh
@@ -1,6 +1,5 @@
 find private/ -type d -exec chmod 750 {} \;
 find private/ -type f -exec chmod 640 {} \;
-chmod 750 private/bin/futoshiki-solver
 
 find public/ -type d -exec chmod 755 {} \;
 find public/ -type f -exec chmod 644 {} \;
